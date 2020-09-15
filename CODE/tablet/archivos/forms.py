6339f9a3c8498d5975121bfce819d203ce42f2d6
RodrigoGarridoLeiva <someone@someplace.com>
2020-09-14 19:16:35 -0500

    Se arreglan pestañas segun casos de uso

--- CODE/tablet/archivos/forms.py
+++ CODE/tablet/archivos/forms.py
@@ -4,9 +4,10 @@ from .models import Archivo
 class DocumentForm(forms.ModelForm):
     class Meta:
         model = Archivo
-        fields = ('nombre', 'file', 'curso_id')
+        fields = ('nombre', 'file', 'curso_id', 'des')
 
         widgets={
         'nombre':forms.TextInput(attrs={'class':'form-control','placeholder':'Inserte Nombre de Archivo'}),
+        'des':forms.TextInput(attrs={'class':'form-control','placeholder':'Descripción para el archivo'}),
         }
 
--- CODE/tablet/archivos/models.py
+++ CODE/tablet/archivos/models.py
@@ -5,6 +5,8 @@ class Archivo(models.Model):
         file = models.FileField(upload_to='archivos/')
         uploaded_at = models.DateTimeField(auto_now_add=True)
         curso_id = models.IntegerField()
+        des = models.CharField(max_length= 100, default='Sin descripción')
+
        
         def __str__(self):
         	return "{}".format(self.nombre)
